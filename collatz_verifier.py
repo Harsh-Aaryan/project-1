9e1f22700e6c409c6903e0a67a14760800d2c1ae
Harsh Aaryan <hah3m@umsystem.edu>
2023-11-28 12:26:28 -0600

    Update file collatz_verifier.py

--- collatz_verifier.py
+++ collatz_verifier.py
@@ -28,17 +28,23 @@ def collatz_verifier(l: list[int]) -> list[list[int]]:
     # Your code goes here >>>>>
 
     sequences = []
+    cache = {}
 
-    for num in nums:
+    for num in l:
         sequence = [num]
         while num != 1:
-            if num % 2 == 0:
-                num //= 2
+            if num in cache:
+                num = cache[num]
             else:
-                num = 3 * num + 1
+                if num % 2 == 0:
+                    num //= 2
+                else:
+                    num = 3 * num + 1
+                cache[num] = num
             sequence.append(num)
         sequences.append(sequence)
     return sequences
+
     # <<<<<
 
 
